6a3244d9d25d28feb3a7353b61411559a8f260fc
Kanish Kumar <kanishkumar@my.com>
2022-08-10 15:02:08 +0530

    .

---
+++ main.py
@@ -0,0 +1,78 @@
+import mysql
+from sqlite3 import Cursor
+import mysql.connector
+from mysql.connector import Error
+
+
+def con(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            passwd = user_password
+        )
+        print("success")
+    except Error as err:
+        print("Error")
+    return connection
+
+pw = "it9aUM7o8ohnY5FBtgEd"
+db = "boqu686i3arvqfcd306c"
+
+connection = con("boqu686i3arvqfcd306c-mysql.services.clever-cloud.com","uj21uhyj8bu3xhp5",pw)
+
+#db connection
+def condb(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            passwd = user_password,
+            database = db_name)
+        print("db connectd")
+    except Error as err:
+        print("error2{err}")
+    return connection
+
+
+#sql commands
+
+def query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("qsucces")
+    except Error as err:
+        print("error3")
+
+roll = str(input("Enter roll no\n"))
+def read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print("error4 '{err}'")
+
+#Getting Data
+q1 = 'select * from result12 where Roll =' +roll+';'
+connection = condb("boqu686i3arvqfcd306c-mysql.services.clever-cloud.com","uj21uhyj8bu3xhp5",pw,db)
+results = read_query(connection, q1)
+for result in results:
+    print("Name: "+result[2])
+    print("Gender: "+result[1])
+    print("Roll no: "+str(result[0]))
+    print("SUB CODE: "+str(result[3])+" Marks: "+ str(result[4])+" GRADE: "+result[5])
+    print("SUB CODE: "+str(result[6])+" Marks: "+ str(result[7])+" GRADE: "+result[8])
+    print("SUB CODE: "+str(result[9])+" Marks: "+ str(result[10])+" GRADE: "+result[11])
+    print("SUB CODE: "+str(result[12])+" Marks: "+ str(result[13])+" GRADE: "+result[14])
+    print("SUB CODE: "+str(result[15])+" Marks: "+ str(result[16])+" GRADE: "+result[17])
+    print("SUB CODE: "+str(result[18])+" Marks: "+ str(result[19])+" GRADE: "+result[20])
+    print("Result: " + result[21])
+
+input()
